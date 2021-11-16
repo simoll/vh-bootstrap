ca7f4669aa7564a6c84e329a8b0af9612c1be260
Simon Moll <simon.moll@emea.nec.com>
2021-11-16 14:42:23 +0100

    build/install cmake,ninja,clang

--- setup_vh.sh
+++ setup_vh.sh
@@ -3,22 +3,43 @@ GITHUB=https://github.com
 DIR=$(dirname $(readlink -f $0))
 
 # Prepare environment
-PREIFX=${HOME}/install
+PREFIX=${HOME}/install
 mkdir -p ${PREFIX}
 
-CMAKE_SRC=${DIR}/cmake-crt
-NINJA_SRC=${DIR}/cmake-crt
-LLVM_SRC=${DIR}/cmake-crt
+CMAKE_SRC=${DIR}/cmake-src
+NINJA_SRC=${DIR}/ninja-src
+LLVM_SRC=${DIR}/llvm-project
 
 # CMake
 git clone ${GITHUB}/KitWare/CMake.git -b release ${CMAKE_SRC}
 mkdir -p ${CMAKE_SRC}/build
-cd ${CMAKE_SRC}/build && ./bootstrap -DCMAKE_INSTALL_PREFIX=${PREFIX} && make && make install
-
+cd ${CMAKE_SRC}/build && ../bootstrap --prefix=${PREFIX} && make && make install
+CMAKE=${PREFIX}/bin/cmake
 
 # Ninja
+git clone ${GITHUB}/ninja-build/ninja -b release ${NINJA_SRC}
+mkdir -p ${NINJA_SRC}/build
+cd ${NINJA_SRC}/build && ${CMAKE} -DCMAKE_INSTALL_PREFIX=${PREFIX} ../ && make && make install
+NINJA=${PREFIX}/bin/ninja
 
-git clone ${GITHUB}/llvm/llvm-project.git -b release/12.x
+
+# LLVM
+git clone ${GITHUB}/llvm/llvm-project.git -b release/12.x ${LLVM_SRC}
+LLVM_BUILD=${DIR}/build_llvm
+mkdir -p ${LLVM_BUILD}
+cd ${LLVM_BUILD} && ${CMAKE} -G Ninja ${LLVM_SRC}/llvm \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_OPTIMIZED_TABLEGEN=Yes \
+    -DLLVM_TARGETS_TO_BUILD=X86 \
+    -DCMAKE_CXX_FLAGS="-O3 -DNDEBUG -march=native" \
+    -DCMAKE_C_FLAGS="-O3 -DNDEBUG -march=native" \
+    -DLLVM_ENABLE_PROJECTS="clang;lld" \
+    -DCLANG_DEFAULT_LINKER="lld"
+
+cd ${LLVM_BUILD} && ${NINJA}
+cd ${LLVM_BUILD} && ${NINJA} install
+
 
 
 
@@ -27,5 +48,7 @@ INIT_SCRIPT=${HOME}/.prefix.sh
 
 cat > ${INIT_SCRIPT} << EOF
 export PATH=${PREFIX}/install/bin:${PATH}
-export LD_LIBRARY_PATH=${PREFIX}/install/bilib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${PREFIX}/install/lib:${LD_LIBRARY_PATH}
+export CC=${PREFIX}/bin/clang
+export CXX=${PREFIX}/bin/clang++
 EOF
